f1b1dbd4ee3fab648429d73604ead1c7f1bb8d4e
rol506 <rolromva@gmail.com>
2024-01-08 15:52:54 +0200

    fixed sounds on surfaces

--- Assets/scripts/SurfaceAudio.cs
+++ Assets/scripts/SurfaceAudio.cs
@@ -1,14 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SurfaceAudio : MonoBehaviour
 {
     public FirstPersonAudio firstPersonAudio;
 
-    [Header("Audio")]
-    public AudioSource stepsMetal;
-    public AudioSource defaultStep;
+    [Header("Default")]
+    public AudioClip[] defaultSteps;
+    public AudioClip[] defaultLanding;
+    public AudioClip[] defaultJump;
+
+    [Header("Metal")]
+    public AudioClip[] metalSteps;
+    public AudioClip[] metalLanding;
+    public AudioClip[] metalJump;
+
+    [Header("Water")]
+    public AudioClip[] waterSteps;
+    public AudioClip[] waterLanding;
+    public AudioClip[] waterJump;
+
+    [Header("Stone")]
+    public AudioClip[] stoneSteps;
+    public AudioClip[] stoneLanding;
+    public AudioClip[] stoneJump;
+
+    private Jump jump;
+
+    private void Start()
+    {
+        jump = GetComponent<Jump>();
+    }
 
     void Update()
     {
@@ -17,10 +41,25 @@ public class SurfaceAudio : MonoBehaviour
         {
             if (hit.collider.tag == "SurfaceMetal")
             {
-                firstPersonAudio.stepAudio = stepsMetal;
-            } else
+                firstPersonAudio.stepAudio.clip = metalSteps[Random.Range(0, metalSteps.Length)];
+                firstPersonAudio.landingSFX = metalLanding;
+                firstPersonAudio.jumpSFX = metalJump;
+            } else if (hit.collider.tag == "SurfaceWater")
             {
-                firstPersonAudio.stepAudio = defaultStep;
+                firstPersonAudio.stepAudio.clip = waterSteps[Random.Range(0, waterSteps.Length)];
+                firstPersonAudio.landingSFX = waterLanding;
+                firstPersonAudio.jumpSFX = waterJump;
+            } else if (hit.collider.tag == "SurfaceStone")
+            {
+                firstPersonAudio.stepAudio.clip = stoneSteps[Random.Range(0, stoneSteps.Length)];
+                firstPersonAudio.landingSFX = stoneLanding;
+                firstPersonAudio.jumpSFX = stoneJump;
+            }
+            else
+            {
+                firstPersonAudio.stepAudio.clip = defaultSteps[Random.Range(0, defaultSteps.Length)];
+                firstPersonAudio.landingSFX = defaultLanding;
+                firstPersonAudio.jumpSFX = defaultJump;
             }
         }
     }
