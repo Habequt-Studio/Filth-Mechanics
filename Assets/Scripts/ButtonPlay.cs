6077b26d9da0e5b866a22f05a611b6e3f56826b9
Profi1Paint <Diflur267@gmail.com>
2024-12-21 23:50:28 +0300

    Function_for_button_play

---
+++ Assets/Scripts/ButtonPlay.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPlay : MonoBehaviour
+{
+public void OnClick()
+{
+SceneManagement.SceneManager.LoadScene(2);
+}
+
+}
