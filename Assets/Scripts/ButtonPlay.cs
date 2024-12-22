ae0f7b09f48cfb4572f33c40dfeeab564c64b5d9
Profi1Paint <Diflur267@gmail.com>
2024-12-22 12:58:34 +0300

    Lamp

--- Assets/Scripts/ButtonPlay.cs
+++ Assets/Scripts/ButtonPlay.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class ButtonPlay : MonoBehaviour
 {
 public void OnClick()
 {
-SceneManagement.SceneManager.LoadScene(2);
+SceneManager.LoadScene(2);
 }
 
 }
