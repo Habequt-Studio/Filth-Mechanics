1601c297228cfd70099c161aa7fe39672f7178d0
Profi1Paint <Diflur267@gmail.com>
2024-12-28 22:18:13 +0300

    Added_animation_for_helicopter_cabin

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Blink : MonoBehaviour
+{
+    public static float delay = 0;
+    Image img;
+
+    private void Start()
+    {
+        img = GetComponent<Image>();
+        StartCoroutine(fade());
+    }
+
+    IEnumerator fade()
+    {
+        yield return new WaitForSeconds(delay);
+        delay = 0;
+        Color col = Color.black;
+        while(col.a > 0)
+        {
+            col.a -= 0.02f;
+            img.color = col;
+            yield return new WaitForSecondsRealtime(0.05f);
+        }
+        Destroy(gameObject);
+    }
+
+}
