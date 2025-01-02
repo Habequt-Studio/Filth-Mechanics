9710c0307e2a544c94b8078c0b087440d5cf2ea2
rol506 <rolromva@gmail.com>
2025-01-02 11:10:44 +0200

    Added heart to the HUD

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,28 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private int health;
-    private int maxHealth = 20;
+    [SerializeField] private int maxHealth = 20;
+
+    [SerializeField] private Image healthImage;
+
+    //health icons
+    [SerializeField] private Sprite HP_full; //full
+    [SerializeField] private Sprite HP_lower; //lower
+    [SerializeField] private Sprite HP_half; //half
+    [SerializeField] private Sprite HP_lowest; //lowest
 
     private void Start()
     {
         health = maxHealth;
-    }
-
-    private void Update()
-    {
-        if (health <= 0)
-        {
-            Die();
-        }
+        updateHealth(); //ladno
     }
 
     public static void Die()
     {
         print("Player died");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //reload scene
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        updateHealth();
+    }
+
+    private void updateHealth()
+    {
+        if (health <= 0)
+        {
+            Die();
+        } else if (health <= maxHealth/4)
+        {
+            healthImage.sprite = HP_lowest;
+        } else if (health <= maxHealth/2)
+        {
+            healthImage.sprite = HP_half;
+        } else if (health <= maxHealth/4*3)
+        {
+            healthImage.sprite = HP_lower;
+        } else //(health >= maxHealth)
+        {
+            healthImage.sprite = HP_full;
+        }
     }
 }
