06cbf79897eb667e6594f2e11bdae0258e8a48aa
Profi1Paint <Diflur267@gmail.com>
2025-01-19 12:28:53 +0300

    fixed

--- Assets/Scripts/Avtomat.cs
+++ Assets/Scripts/Avtomat.cs
@@ -13,16 +13,19 @@ public class Avtomat1 : MonoBehaviour {
   public Transform spawn;
   public TMP_Text Ammo_text;
   public int MaxAmmo = 21;
+  public GameObject Avtomat;
 
   void Update () {
     if (Input.GetMouseButtonDown (0) && CurrentAmmo > 0) {
+    Avtomat.GetComponent<Animator>(). SetTrigger ("Shot");
       Transform BulletInstance = (Transform)Instantiate (bullet, spawn.position, Quaternion.identity);
       BulletInstance.GetComponent<Rigidbody> ().AddForce (transform.forward * (BulletForce * -1));
       CurrentAmmo = CurrentAmmo - 1;
       GetComponent<AudioSource> ().PlayOneShot (Fire);
-      GetComponent<AudioSource> ().PlayOneShot (Reload);
     }
     if (Input.GetKeyDown (KeyCode.R)) {
+    GetComponent<AudioSource> ().PlayOneShot (Reload);
+    Avtomat.GetComponent<Animator>(). SetTrigger ("Reloaded");
       MaxAmmo = MaxAmmo - 15;
       CurrentAmmo = 15;
     }
