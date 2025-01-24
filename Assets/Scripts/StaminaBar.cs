4f10c034bff82bd5a578111ec3d8b992b7b0afe0
Profi1Paint <Diflur267@gmail.com>
2025-01-24 12:28:12 +0300

    UP

--- Assets/Scripts/StaminaBar.cs
+++ Assets/Scripts/StaminaBar.cs
@@ -5,6 +5,24 @@ using UnityEngine.UI;
 
 public class StaminaBar : MonoBehaviour
 {
+ [Header("Stamina main parametrs")]
+ public float playerStamina = 100.0f;
+ [SerializeField] private float maxStamina = 100.0f;
+ [SerializeField] private float JumpCost = 20;
+ [HideInInspector] public bool HasReGenerated = true;
+ [HideInInspector] public bool WeAreSpriting = false;
+
+ [Header("Stamina Regen parametrs")]
+ [Range(0, 50)] [SerializeField] private float staminaDrain = 0.5f;
+ [Range(0, 50)] [SerializeField] private float staminaRegen = 0.5f;
+
+ [Header("Stamina Speed parametrs")]
+ [SerializeField] private int slowedRunSpeed = 4;
+ [SerializeField] private int normalRunSpeed = 8;
+
+ [Header("Stamina UI elements")]
+ [SerializeField] private Image StaminaProgressUI = null;
+ [SerializeField] private CanvasGroup SliderCanvasGroup = null;
     void Start()
     {
         
